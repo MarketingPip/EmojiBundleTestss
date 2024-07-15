033252e28051ac8b47555184b6387b455b8097f5
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2024-07-15 00:59:59 -0400

    Create license.config.js

---
+++ src/license.config.js
@@ -0,0 +1,13 @@
+const packageData = require('../package.json');
+
+const LICENSE = `/**!
+ * @license Emoji-Fallback.js - ${packageData.description}
+ * VERSION: ${packageData.version}
+ * LICENSED UNDER ${packageData?.license} LICENSE
+ * MORE INFO CAN BE FOUND AT https://github.com/MarketingPipeline/Emoji-Fallback.js/
+ */ `;
+
+
+const FILENAME = "emojiFallback" // used for output file name
+
+export const packageCONFIG = {LICENSE,FILENAME}
