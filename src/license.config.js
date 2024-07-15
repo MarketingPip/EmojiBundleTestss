e6e46772b043487f025f84ace48ab6fd38b8007c
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2024-07-15 02:55:54 -0400

    Update license.config.js

--- src/license.config.js
+++ src/license.config.js
@@ -14,6 +14,6 @@ const LICENSE = `/**!
  * MORE INFO CAN BE FOUND AT https://github.com/MarketingPipeline/${REPONAME}/
  */`;
 
-const packageCONFIG = {LICENSE};
+const packageCONFIG = {LICENSE, FILENAME:packageData.main.split("/").pop()};
 
 module.exports = packageCONFIG
