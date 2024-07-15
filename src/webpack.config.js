026853dd1e6d912a086974b1b307d7c5d386e190
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2024-07-14 23:51:54 -0400

    Update webpack.config.js

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -6,6 +6,22 @@ const packageData = require('../package.json');
 
 const TerserPlugin = require('terser-webpack-plugin');
 
+// taken from https://github.com/webpack/webpack/issues/12506#issuecomment-1360810560
+class RemoveLicenseFilePlugin {
+    apply(compiler) {
+        compiler.hooks.emit.tap("RemoveLicenseFilePlugin", (compilation) => {
+            // compliation has assets to output
+            // console.log(compilation.assets);
+            for (let name in compilation.assets) {
+                if (name.endsWith("LICENSE.txt")) {
+                    delete compilation.assets[name];
+                }
+            }
+        });
+    }
+}
+
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -22,7 +38,8 @@ module.exports = {
     minimizer: [new TerserPlugin({
       extractComments: false,
     })],
-  },  
+  },
+  plugins: [new RemoveLicenseFilePlugin()],
   module: {
     rules: [
       {
