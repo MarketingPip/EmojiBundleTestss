4ad4eabcd442c8171d7b05fa66c4e27ab95ff3e7
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2024-07-14 23:29:35 -0400

    Update webpack.config.js

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '..', 'dist'),
     filename: 'bundle.js',
     library: {
       type: 'module',
